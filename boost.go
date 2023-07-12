16401ac12ac594e4c6bf13d944a6c142e36d7d6c
lazy_owl <slimsly11@gmail.com>
2023-07-12 14:13:40 +0600

    fix(handler): fix boost.HandlerFunc

--- boost.go
+++ boost.go
@@ -3,7 +3,6 @@ package boost
 import (
 	"github.com/lowl11/boost/internal/fast_handler"
 	"github.com/lowl11/boost/pkg/boost_context"
-	"github.com/lowl11/boost/pkg/boost_handler"
 	"github.com/lowl11/lazylog/log/log_internal"
 )
 
@@ -20,6 +19,6 @@ func New() *App {
 }
 
 type (
-	HandlerFunc = boost_handler.HandlerFunc
+	HandlerFunc = func(ctx Context) error
 	Context     = boost_context.Context
 )
