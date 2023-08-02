ffa7b63b4d6f63b144294b90395dce986734249b
lazy_owl <slimsly11@gmail.com>
2023-08-02 15:39:50 +0600

    feat(auth): add password encryptor

---
+++ pkg/auth/password/encryptor.go
@@ -0,0 +1,62 @@
+package password
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"github.com/lowl11/boost/pkg/types"
+	"io"
+)
+
+type Encryptor struct {
+	key []byte
+}
+
+func NewEncryptor(key string) *Encryptor {
+	return &Encryptor{
+		key: types.ToBytes(key),
+	}
+}
+
+func (encrypt Encryptor) Encrypt(password string) (string, error) {
+	plaintext := types.ToBytes(password)
+
+	block, err := aes.NewCipher(encrypt.key)
+	if err != nil {
+		return "", ErrorEncryptPassword(err)
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+
+	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+
+func (encrypt Encryptor) Decrypt(password string) (string, error) {
+	ciphertext, _ := base64.URLEncoding.DecodeString(password)
+
+	block, err := aes.NewCipher(encrypt.key)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return "", ErrorDecryptPassword(err)
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return types.ToString(ciphertext), nil
+}
--- pkg/auth/password/errors.go
+++ pkg/auth/password/errors.go
@@ -7,6 +7,8 @@ import (
 
 const (
 	typeErrorPasswordsNotEqual = "PasswordsNotEqual"
+	typeErrorEncryptPassword   = "EncryptPassword"
+	typeErrorDecryptPassword   = "DecryptPassword"
 )
 
 func ErrorPasswordsNotEqual() error {
@@ -14,4 +16,20 @@ func ErrorPasswordsNotEqual() error {
 		New("Passwords are not equal").
 		SetType(typeErrorPasswordsNotEqual).
 		SetHttpCode(http.StatusUnprocessableEntity)
+}
+
+func ErrorEncryptPassword(err error) error {
+	return errors.
+		New("Encrypt password error").
+		SetType(typeErrorEncryptPassword).
+		SetHttpCode(http.StatusInternalServerError).
+		SetError(err)
+}
+
+func ErrorDecryptPassword(err error) error {
+	return errors.
+		New("Decrypt password error").
+		SetType(typeErrorDecryptPassword).
+		SetHttpCode(http.StatusInternalServerError).
+		SetError(err)
 }
