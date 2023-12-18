12fadf48af841a9f169d83535f893d98c7d8325b
lazy_owl <slimsly11@gmail.com>
2023-12-18 12:30:24 +0600

    feat(password): password generator

--- pkg/web/auth/password/static.go
+++ pkg/web/auth/password/static.go
@@ -1,9 +1,40 @@
 package password
 
+import (
+	"github.com/lowl11/boost/pkg/system/types"
+	"math/rand"
+)
+
 func Compare(password, rePassword string) error {
 	if password == rePassword {
 		return nil
 	}
 
 	return ErrorPasswordsNotEqual()
+}
+
+const (
+	letterBytes  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	specialBytes = "!@#$%^&*()_+-=[]{}\\|;':\",.<>/?`~"
+	numBytes     = "0123456789"
+)
+
+func Generate(length int, useLetters, includeSpecial, includeNumber bool) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var password []byte
+	var charSource string
+
+	if includeNumber {
+		charSource += "0123456789"
+	}
+	if includeSpecial {
+		charSource += "!@#$%^&*()_+=-"
+	}
+	charSource += charset
+
+	for i := 0; i < length; i++ {
+		randNum := rand.Intn(len(charSource))
+		password = append(password, charSource[randNum])
+	}
+	return types.ToString(password)
 }
