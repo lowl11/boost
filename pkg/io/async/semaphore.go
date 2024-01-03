9650983c540f77e8500d31bc5474d56eafabb7e0
lazy_owl <slimsly11@gmail.com>
2024-01-03 11:17:27 +0600

    refactor(semaphore): private channel at semaphore

--- pkg/io/async/semaphore.go
+++ pkg/io/async/semaphore.go
@@ -3,19 +3,19 @@ package async
 import "github.com/lowl11/boost/data/interfaces"
 
 type Semaphore struct {
-	C chan struct{}
+	c chan struct{}
 }
 
 func (semaphore *Semaphore) Acquire() {
-	semaphore.C <- struct{}{}
+	semaphore.c <- struct{}{}
 }
 
 func (semaphore *Semaphore) Release() {
-	<-semaphore.C
+	<-semaphore.c
 }
 
 func NewSemaphore(size int) interfaces.Semaphore {
 	return &Semaphore{
-		C: make(chan struct{}, size),
+		c: make(chan struct{}, size),
 	}
 }
