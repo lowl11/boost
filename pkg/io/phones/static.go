0935d7a001dc4c7241df2f613b78a567f1a70608
lazy_owl <slimsly11@gmail.com>
2024-01-05 11:39:16 +0600

    refactor(phones): ref

--- pkg/io/phones/static.go
+++ pkg/io/phones/static.go
@@ -10,17 +10,23 @@ var (
 	removeOddSymbols = regexp.MustCompile("[^+\\d]")
 )
 
-func CheckPhone(phone string) error {
+func Check(phone string) error {
 	// check length (min: 10, max: 20)
 	length := utf8.RuneCountInString(phone)
 	if length < 10 || length > 20 {
 		return errors.New("does not match phone pattern")
 	}
 
+	// todo: check for code
+
 	return nil
 }
 
-func MaskPhone(phone string) string {
+func Is(phone string) bool {
+	return Check(Mask(phone)) == nil
+}
+
+func Mask(phone string) string {
 	phone = removeOddSymbols.ReplaceAllString(phone, "")
 
 	length := utf8.RuneCountInString(phone)
