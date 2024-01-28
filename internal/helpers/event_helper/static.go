7337fc8c7548a9c9bde5df7cdccd1ceea282e042
lazy_owl <slimsly11@gmail.com>
2024-01-28 16:32:24 +0600

    refactor(dispatcher): remove comment

--- internal/helpers/event_helper/static.go
+++ internal/helpers/event_helper/static.go
@@ -6,14 +6,6 @@ import (
 )
 
 func NameOfEvent(event any) (string, error) {
-	// func getType(myvar interface{}) string {
-	//    if t := reflect.TypeOf(myvar); t.Kind() == reflect.Ptr {
-	//        return "*" + t.Elem().Name()
-	//    } else {
-	//        return t.Name()
-	//    }
-	//}
-
 	reflectValue := reflect.TypeOf(event)
 	if reflectValue.Kind() != reflect.Struct {
 		return "", errors.New("given argument is not struct{}")
