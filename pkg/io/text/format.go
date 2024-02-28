d0269aacabe1e685b96dec0457182a7c073256de
lazy_owl <slimsly11@gmail.com>
2024-02-28 14:01:25 +0600

    feat(text): add Username func

--- pkg/io/text/format.go
+++ pkg/io/text/format.go
@@ -3,8 +3,13 @@ package text
 import (
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
+	"regexp"
 	"strings"
 	"unicode"
+)
+
+var (
+	_manySpacesReg = regexp.MustCompile("\\s{2,}")
 )
 
 // Title makes text first word starts with upper case, other words with lower case.
@@ -30,4 +35,33 @@ func Title(text string) string {
 //	Output: Hello World
 func EveryTitle(text string) string {
 	return cases.Title(language.Und).String(strings.ToLower(text))
+}
+
+// Username makes given text to username standard.
+// Example:
+//
+// Input: John Smith
+// Output: john_smith
+func Username(username string) string {
+	username = strings.TrimSpace(username)
+	username = strings.ToLower(username)
+	username = _manySpacesReg.ReplaceAllString(username, " ")
+	username = strings.ReplaceAll(username, " ", "_")
+	username = OnlyLetter(username)
+	return username
+}
+
+// OnlyLetter keeps only latin letters & underscore ("_")
+// Example:
+//
+// Input: john_smith_jr.
+// Output: john_smith_jr
+func OnlyLetter(text string) string {
+	var output []rune
+	for _, char := range text {
+		if unicode.IsLetter(char) || char == '_' {
+			output = append(output, char)
+		}
+	}
+	return string(output)
 }
