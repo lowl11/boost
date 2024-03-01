a5cbff7c99ed138c8b2193b1354c8110292d2f9a
lazy_owl <slimsly11@gmail.com>
2024-03-01 17:21:22 +0500

    refactor(di): refactor

--- pkg/system/di/di.go
+++ pkg/system/di/di.go
@@ -15,18 +15,8 @@ func Get[T any](params ...any) *T {
 	return object.(*T)
 }
 
-func AddTransient[T any](constructor any, dependencies ...any) {
+func RegisterTransient[T any](constructor any, dependencies ...any) {
 	di_container.Get().Register(reflect.TypeOf(new(T)), constructor, di_modes.Transient, dependencies...)
-}
-
-func RegisterScoped[T any](constructor any, dependencies ...any) {
-	di_container.Get().Register(reflect.TypeOf(new(T)), constructor, di_modes.Scoped, dependencies...)
-}
-
-// AddSingleton registers objects in "singleton" mode.
-// Deprecated: will be removed since 0.6.0 and higher
-func AddSingleton[T any](constructor any, dependencies ...any) {
-	di_container.Get().Register(reflect.TypeOf(new(T)), constructor, di_modes.Singleton, dependencies...)
 }
 
 // Register registers object in "singleton" mode
